096d28518beffe2996e38d73a390c0b577764195
chjrwithac <chjrwithac@gmail.com>
2019-12-17 07:50:31 +0800

    Calibration complete

--- a_GLOBAL/e_SETUP.ino
+++ a_GLOBAL/e_SETUP.ino
@@ -13,43 +13,43 @@ void setup() {
 
   // Setting signal reference points //
   
-  L_1.setCoxaSignalMin(0);
-  L_1.setCoxaSignalMax(0);
+  L_1.setCoxaSignalMin(420);
+  L_1.setCoxaSignalMax(228);
   L_1.setFemurSignalMin(120);
   L_1.setFemurSignalMax(296);
   L_1.setTibiaSignalMin(304);
   L_1.setTibiaSignalMax(124);
   
-  L_2.setCoxaSignalMin(0);
-  L_2.setCoxaSignalMax(0);
+  L_2.setCoxaSignalMin(388);
+  L_2.setCoxaSignalMax(248);
   L_2.setFemurSignalMin(120);
   L_2.setFemurSignalMax(300);
   L_2.setTibiaSignalMin(320);
   L_2.setTibiaSignalMax(152);
   
-  L_3.setCoxaSignalMin(0);
-  L_3.setCoxaSignalMax(0);
+  L_3.setCoxaSignalMin(404);
+  L_3.setCoxaSignalMax(212);
   L_3.setFemurSignalMin(156);
   L_3.setFemurSignalMax(340);
   L_3.setTibiaSignalMin(332);
   L_3.setTibiaSignalMax(148);
 
-  R_1.setCoxaSignalMin(0);
-  R_1.setCoxaSignalMax(0);
+  R_1.setCoxaSignalMin(412);
+  R_1.setCoxaSignalMax(216);
   R_1.setFemurSignalMin(150);
   R_1.setFemurSignalMax(332);
   R_1.setTibiaSignalMin(344);
   R_1.setTibiaSignalMax(148);
   
-  R_2.setCoxaSignalMin(0);
-  R_2.setCoxaSignalMax(0);
+  R_2.setCoxaSignalMin(388);
+  R_2.setCoxaSignalMax(240);
   R_2.setFemurSignalMin(144);
   R_2.setFemurSignalMax(336);
   R_2.setTibiaSignalMin(328);
   R_2.setTibiaSignalMax(120);
   
-  R_3.setCoxaSignalMin(0);
-  R_3.setCoxaSignalMax(0);
+  R_3.setCoxaSignalMin(400);
+  R_3.setCoxaSignalMax(212);
   R_3.setFemurSignalMin(140);
   R_3.setFemurSignalMax(320);
   R_3.setTibiaSignalMin(300);
@@ -57,22 +57,22 @@ void setup() {
 
   // Setting joint reference points // 
   
-  L_1.setCoxaRadMin(0);
-  L_1.setCoxaRadMax(0);
+  L_1.setCoxaRadMin(radians(180));
+  L_1.setCoxaRadMax(radians(90));
   L_1.setFemurRadMin(radians(-90));
   L_1.setFemurRadMax(radians(0));
   L_1.setTibiaRadMin(radians(-90));
   L_1.setTibiaRadMax(radians(0));
   
-  L_2.setCoxaRadMin(0);
-  L_2.setCoxaRadMax(0);
+  L_2.setCoxaRadMin(radians(213.5));
+  L_2.setCoxaRadMax(radians(146.5));
   L_2.setFemurRadMin(radians(-90));
   L_2.setFemurRadMax(radians(0));
   L_2.setTibiaRadMin(radians(-90));
   L_2.setTibiaRadMax(radians(0));
  
-  L_3.setCoxaRadMin(0);
-  L_3.setCoxaRadMax(0);
+  L_3.setCoxaRadMin(radians(180));
+  L_3.setCoxaRadMax(radians(270));
   L_3.setFemurRadMin(radians(-90));
   L_3.setFemurRadMax(radians(0));
   L_3.setTibiaRadMin(radians(-90));
@@ -85,15 +85,15 @@ void setup() {
   R_1.setTibiaRadMin(radians(-90));
   R_1.setTibiaRadMax(radians(0));
   
-  R_2.setCoxaRadMin(0);
-  R_2.setCoxaRadMax(0);
+  R_2.setCoxaRadMin(radians(-33.5));
+  R_2.setCoxaRadMax(radians(33.5));
   R_2.setFemurRadMin(radians(-90));
   R_2.setFemurRadMax(radians(0));
   R_2.setTibiaRadMin(radians(-90));
   R_2.setTibiaRadMax(radians(0));
  
-  R_3.setCoxaRadMin(0);
-  R_3.setCoxaRadMax(0);
+  R_3.setCoxaRadMin(radians(-90));
+  R_3.setCoxaRadMax(radians(0));
   R_3.setFemurRadMin(radians(-90));
   R_3.setFemurRadMax(radians(0));
   R_3.setTibiaRadMin(radians(-90));
--- a_GLOBAL/f_MAIN.ino
+++ a_GLOBAL/f_MAIN.ino
@@ -1,19 +1,4 @@
 
 void loop() {
-  copyVolatileSignalsToArray(DEBUG_SIGNAL_ARRAY);
-  float command_signal = getSignalFromArray(DEBUG_SIGNAL_ARRAY, ch_3);
   
-  float command_signal_offset = -signal_min;
-  float adjusted_command_signal = command_signal + command_signal_offset;
-  float adjusted_signal_min = signal_min + command_signal_offset;
-  float adjusted_signal_max = signal_max + command_signal_offset;
-
-  // FOR FINDING SIGNAL LIMITS //
-  R_1.moveCoxaToSignal(adjusted_command_signal);
-  Serial.println(adjusted_command_signal);
-
-  // FOR TESTING SIGNAL LIMITS //
-//  float command_angle = floatMap(command_signal, signal_min, signal_max, L_1.getTibiaRadMin(), L_2.getTibiaRadMax());
-//  L_1.moveTibiaToAngle(command_angle);
-//  Serial.println(command_angle);
 }
